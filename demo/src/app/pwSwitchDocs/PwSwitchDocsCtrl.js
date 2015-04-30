fe1fcc24e39d2f1d391bcf156c5e9c3194e24937
Pedro Catré <pedro.catre@payworksmobile.com>
2015-04-30 18:40:53 +0200

    Added more colors to the demo

--- demo/src/app/pwSwitchDocs/PwSwitchDocsCtrl.js
+++ demo/src/app/pwSwitchDocs/PwSwitchDocsCtrl.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('pwSwitchDocs')
-  .controller('PwSwitchDocsCtrl', function ($scope) {
+  .controller('PwSwitchDocsCtrl', function () {
     var vm = this;
     vm.switch1Value = 'ACTIVE';
     vm.switch2Value = 'DEACTIVATED';
+    vm.switch4Value = 'ACTIVE';
     vm.switch3Value = true;
   });
